03b43eaadf125f1590653fbb3fb660d75b816e78
Rjack00 <rjackson77@comcast.net>
2024-05-28 18:35:44 -0600

    Updated script.js
    Added the following functions: pauseSong, playNextSong, playPreviousSong, getCurrentSongIndex. Added eventListeners for pauseButton, nextButton and previousButton.

--- script.js
+++ script.js
@@ -100,7 +100,35 @@ const playSong = (id) => {
     playButton.classList.add("playing");
   
     audio.play();
-  };
+};
+
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    
+    playButton.classList.remove("playing");
+    audio.pause();
+};
+
+const playNextSong = () => {
+
+    if (userData?.currentSong === null) {
+      playSong(userData?.songs[0].id);
+    } else {
+      const currentSongIndex = getCurrentSongIndex();
+      const nextSong = userData?.songs[currentSongIndex + 1];
+  
+      playSong(nextSong.id);
+    }
+};
+
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex -1];
+    playSong(previousSong.id);
+  }
+};
 
 const renderSongs = (array) => {
     const songsHTML = array
@@ -124,6 +152,8 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
 playButton.addEventListener("click", () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
@@ -131,6 +161,13 @@ playButton.addEventListener("click", () => {
         playSong(userData?.currentSong.id);
       }
 });
+
+pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
+
 
 const sortSongs = () => {
     userData?.songs.sort((a,b) => {
