b8d05993f203ed3985c8bc9b5ccc46ba670d47be
Rjack00 <rjackson77@comcast.net>
2024-06-01 17:38:23 -0600

    Updated html, js and css
    Updated html title; updated css background colors and button focus; js not updated.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const allSongs = [
     title: "Scratching The Surface",
     artist: "Quincy Larson",
     duration: "4:25",
-    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+    src: "https://music.youtube.com/watch?v=YKRmnxaQlFE&si=2PMB163YxNhn0LAZ",
   },
   {
     id: 1,
